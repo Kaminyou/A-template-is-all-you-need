88464fc9c83e0d0dd9b2483d576b871fdaedb7b5
Kaminyou <ikaminyou@gmail.com>
2021-05-28 17:19:53 +0800

    refined

--- initialize.py
+++ initialize.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     with open("config.json") as f:
         config = json.load(f)
 
-    #os.system(f"wget ftp://cs.stanford.edu/cs/cvgl/PASCAL3D+_release1.1.zip -P {config['pascal_root']}")
+    os.system(f"wget ftp://cs.stanford.edu/cs/cvgl/PASCAL3D+_release1.1.zip -P {config['pascal_root']}")
     os.system(f"unzip {os.path.join(config['pascal_root'], 'PASCAL3D+_release1.1.zip')} -d {config['pascal_root']}")
