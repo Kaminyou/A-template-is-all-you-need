dc7e70685c2818ab6eac676348294a6b3f9eb940
Kaminyou <ikaminyou@gmail.com>
2021-05-28 17:17:33 +0800

    <add >initialize instruction

---
+++ initialize.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+if __name__ == "__main__":
+    if not os.path.exists("config.json"):
+        print("Please provide config.json first!")
+        exit(0)
+
+    with open("config.json") as f:
+        config = json.load(f)
+
+    #os.system(f"wget ftp://cs.stanford.edu/cs/cvgl/PASCAL3D+_release1.1.zip -P {config['pascal_root']}")
+    os.system(f"unzip {os.path.join(config['pascal_root'], 'PASCAL3D+_release1.1.zip')} -d {config['pascal_root']}")
