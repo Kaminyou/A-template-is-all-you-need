4d6ed76e52fb12eb14d0d47b6d39de9b29064793
Kaminyou <ikaminyou@gmail.com>
2021-06-03 10:11:33 +0800

    shapenet

--- initialize.py
+++ initialize.py
@@ -11,3 +11,6 @@ if __name__ == "__main__":
 
     os.system(f"wget ftp://cs.stanford.edu/cs/cvgl/PASCAL3D+_release1.1.zip -P {config['pascal_root']}")
     os.system(f"unzip {os.path.join(config['pascal_root'], 'PASCAL3D+_release1.1.zip')} -d {config['pascal_root']}")
+
+    os.system(f"wget {config['shapenet_url']} -P {config['shapenet_root']}")
+    os.system(f"unzip {os.path.join(config['shapenet_root'], 'ShapeNetCore.v2.zip')} -d {config['shapenet_root']}")
